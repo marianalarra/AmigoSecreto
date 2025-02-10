b68fe5e843ee1d964f05a9068477abbc41ab16d2
Mariana <marianalarra@yahoo.com.ar>
2025-02-10 17:16:21 +0100

    Funcion actualizaListaAmigos() y sortearAmigos()

--- app.js
+++ app.js
@@ -4,14 +4,41 @@ let amigos=[];
 
 function agregarAmigo(){
     let amigo = document.getElementById("amigo").value;
-    if (amigo===""){
+    if (amigo.trim()===""){
         alert("Por favor, inserte un nombre.")
 
     }else{
         amigos.push(amigo);
+        //Borro el campo donde se agregan los amigos
+        document.getElementById("amigo").value = ""; 
+        actualizaListaAmigos(); 
+    }
+    
+    
+   
+}
+
+function actualizaListaAmigos(){
+    //Obtener elemento de la lista
+    let lista = document.getElementById("listaAmigos");
+    //Borro los elementos que hayan quedado de la lista anterior
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const element = amigos[i];
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+      }
     }
 
-    console.log(amigos)
-    document.getElementById("amigo").value='';
-
-}
+    function sortearAmigo() {
+        let cantidadAmigos = amigos.length;
+        if (cantidadAmigos === 0) {
+          alert("Por favor, inserte un nombre antes de sortear");
+        } else {
+          let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+          let resultadoHTML = document.querySelector("#resultado");
+          resultadoHTML.innerHTML = amigos[indiceAmigo];
+        }
+      }
+    
